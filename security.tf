3888e9381d8531fd17ca9bd71f135fd758030f8b
Josik Abdul <jmosquera_1@hotmail.com>
2026-09-11 22:50:51 -0500

    Fix: permitir health check interno del NLB del backend

--- security.tf
+++ security.tf
@@ -17,3 +17,13 @@ resource "aws_security_group_rule" "gateway_allow_nodeport_from_internet" {
   security_group_id = module.eks_gateway.cluster_security_group_id
   description       = "Permite trafico del Load Balancer publico hacia los nodos de eks-gateway"
 }
+
+resource "aws_security_group_rule" "backend_allow_self_healthcheck" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = [module.vpc_backend.vpc_cidr_block]
+  security_group_id = module.eks_backend.cluster_security_group_id
+  description       = "Permite el health check del Load Balancer interno (trafico desde la propia VPC)"
+}
