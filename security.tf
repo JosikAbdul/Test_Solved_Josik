c591a6c8e6e8d55df608f03407f00db90d7f611c
Josik Abdul <jmosquera_1@hotmail.com>
2026-09-11 21:27:17 -0500

    Fix: permitir trafico publico al NodePort de eks-gateway

--- security.tf
+++ security.tf
@@ -7,3 +7,13 @@ resource "aws_security_group_rule" "backend_allow_from_gateway" {
   security_group_id = module.eks_backend.cluster_security_group_id
   description       = "Permite trafico solo desde vpc-gateway hacia eks-backend"
 }
+
+resource "aws_security_group_rule" "gateway_allow_nodeport_from_internet" {
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.eks_gateway.cluster_security_group_id
+  description       = "Permite trafico del Load Balancer publico hacia los nodos de eks-gateway"
+}
